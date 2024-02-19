aa4e0ee0685450e4afe8cdd7596d0ec750292fda
julius-ek-hub <ekanejulius7@gmail.com>
2024-02-19 20:22:15 +0400

    Added header

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,11 @@
-import * as React from 'react';
-import Head from 'next/head';
-import { ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import { CacheProvider } from '@emotion/react';
-import theme from '../src/theme';
-import createEmotionCache from '../src/createEmotionCache';
+import * as React from "react";
+import Head from "next/head";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { CacheProvider } from "@emotion/react";
+import theme from "../src/theme";
+import createEmotionCache from "../src/createEmotionCache";
 
-// Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
 
 export default function MyApp(props) {
@@ -16,9 +15,15 @@ export default function MyApp(props) {
     <CacheProvider value={emotionCache}>
       <Head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <meta name="og:description" content="GHESSAD 2024 Executive Election" />
+        <title>GHESSAD 2024 Executive Election</title>
+        <meta
+          name="og:title"
+          content="GHESSAD 2024 Executive Election, Experience a new regime"
+        />
+        <meta name="image" content="/gessad-logo.jpeg" />
       </Head>
       <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
         <Component {...pageProps} />
       </ThemeProvider>
